81dee93505d6de463aeb2eb948ec3935b06fc88d
trandung2k1 <trandungitksnb2001@gmail.com>
2023-05-08 11:02:21 +0700

    jwt not expired logout problem

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,23 @@
 import { Express, Request, Response } from 'express';
 import auth from './auth.route';
+import jwt from 'jsonwebtoken';
 const routes = (app: Express) => {
     app.use('/api/auth', auth);
     app.post('/login', (req: Request, res: Response) => {
         const refreshToken: string = 'Refresh token';
-        // res.cookie('refreshToken', refreshToken, {
-        //     httpOnly: true,
-        //     path: '/',
-        //     secure: false,
-        //     sameSite: 'strict',
-        //     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-        // });
+        const accessToken: string = jwt.sign({ id: 1 }, 'mySecret', {
+            expiresIn: '60s',
+        });
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            path: '/',
+            secure: false,
+            sameSite: 'strict',
+            expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        });
         return res.status(200).json({
             message: 'Login successfully',
+            accessToken,
         });
     });
     app.get('/getCookie', (req: Request, res: Response) => {
@@ -26,6 +31,27 @@ const routes = (app: Express) => {
             message: 'Refresh token not found',
         });
     });
+    app.get('/data', (req, res) => {
+        const { accessToken } = req.body;
+        // console.log(accessToken);
+        jwt.verify(accessToken, 'mySecret', (err: any, decoded: any) => {
+            if (err?.name === 'TokenExpiredError') {
+                return res.status(400).json({
+                    message: 'Jwt expired',
+                });
+            }
+            if (err?.name === 'JsonWebTokenError') {
+                return res.status(401).json({
+                    message: err.message,
+                });
+            }
+            if (decoded.id === 1) {
+                return res.status(200).json({
+                    message: 'Hello',
+                });
+            }
+        });
+    });
     app.post('/refresh', (req: Request, res: Response) => {
         if (req.cookies.refreshToken) {
             return res.status(200).json({
@@ -37,7 +63,7 @@ const routes = (app: Express) => {
             });
         }
     });
-    app.post('/logout', (req: Request, res: Response) => {
+    app.get('/logout', (req: Request, res: Response) => {
         res.clearCookie('refreshToken');
         return res.status(200).json({
             message: 'Logout successfully',
